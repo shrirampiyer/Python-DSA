d9b47cfbf1331dc17f7488e5dcffef657adb5c37
shrirampiyer <shrirampiyerthepatriot@gmail.com>
2024-03-05 14:56:17 +0530

    added different methods to do linear search

--- linear_search.py
+++ linear_search.py
@@ -1,11 +1,46 @@
 
 def locate_cards(cards, query):
-    for i in cards:
-        if cards[i] == query:
-            print( f"Element {cards[i]}, found at location {i}")
-            return f"Element {cards[i]}, found at location {i}"
-    
-cards = [1,2,3,4,5,6,7,8,9,10]
-query = 9
+    position = 0
+    while True:
+        if cards[position]==query:
+            return position
+        position += 1
+        if len(cards) == position:
+            return -1
 
-locate_cards(cards, query)
+tests = {
+    'input': { 
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 7
+    },
+    'output': 3
+}
+print(locate_cards(**test['input'])==test['output'])
+
+#alternate solution using for loop and avoiding enumarate method 
+def locate_cards(cards, query):
+  pos = 0
+  for i in cards:
+    if i == query:
+      return i,pos
+    pos += 1
+
+cards = [13, 11, 10, 7, 4, 3, 1, 0]
+query = 7
+
+#implementing using enumearate method in for loop
+def locate_cards(cards, query):
+    for i, card in enumerate(cards):
+        if card == query:
+            print(f"Element {query}, found at location {i}")
+            return f"Element {query}, found at location {i}"
+
+tests = {
+    'input': { 
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 7
+    },
+    'output': 'Element 7, found at location 3'
+}
+
+print(locate_cards(**tests['input']) == tests['output'])
